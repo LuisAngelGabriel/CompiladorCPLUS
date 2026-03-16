551ef2635c0d06b0be11776833ecc9348181df68
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-16 10:49:52 -0400

    Agrega el contexto

---
+++ CompiladorCPLUS/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using CompiladorCPLUS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompiladorCPLUS.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Compilacion> Compilaciones { get; set; }
+    }
+}
