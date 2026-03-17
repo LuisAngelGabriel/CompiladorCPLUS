f266f94b9a96e07c10b748c3edf4499416b0ad31
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-03-17 00:11:00 -0400

    migracion

--- CompiladorCPLUS/Migrations/20260317035324_AgregarResultadoAnalisis.Designer.cs
+++ CompiladorCPLUS/Migrations/20260317035324_AgregarResultadoAnalisis.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CompiladorCPLUS.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CompiladorCPLUS.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20260317035324_AgregarResultadoAnalisis")]
+    partial class AgregarResultadoAnalisis
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
---
+++ CompiladorCPLUS/Migrations/20260317035324_AgregarResultadoAnalisis.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompiladorCPLUS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarResultadoAnalisis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResultadoAnalisis",
+                table: "Compilaciones",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResultadoAnalisis",
+                table: "Compilaciones");
+        }
+    }
+}
