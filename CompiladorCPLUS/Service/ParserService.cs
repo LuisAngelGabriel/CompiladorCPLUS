19afc6c4de1922742010b7019355bf46da5e4f2d
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-22 17:56:41 -0400

    Corrige algunos errores con las llaves dentro de la clase Parser

--- CompiladorCPLUS/Service/ParserService.cs
+++ CompiladorCPLUS/Service/ParserService.cs
@@ -12,12 +12,16 @@ public class ParserService
         if (string.IsNullOrWhiteSpace(codigo)) return erroresSintacticos;
 
         string[] lineas = codigo.Split('\n');
+        int balanceLlaves = 0;
 
         for (int i = 0; i < lineas.Length; i++)
         {
             string linea = lineas[i].Trim();
             if (string.IsNullOrEmpty(linea) || linea.StartsWith("//") || linea.StartsWith("#") || linea.StartsWith("using")) continue;
             int numLinea = i + 1;
+
+            balanceLlaves += linea.Count(f => f == '{');
+            balanceLlaves -= linea.Count(f => f == '}');
 
             bool esEstructuraValida = linea.EndsWith(";") || linea.EndsWith("{") || linea.EndsWith("}") ||
                                      linea.Contains("if") || linea.Contains("main()") || linea.Contains("else");
@@ -30,20 +34,27 @@ public class ParserService
             if (linea.Contains("cout") || linea.Contains("cin") || linea.Contains("print") || linea.Contains("input"))
             {
                 if ((linea.Contains("print") || linea.Contains("input")) && (!linea.Contains("(") || !linea.Contains(")")))
+                {
                     erroresSintacticos.Add($"Error Sintáctico (Línea {numLinea}): La función requiere '( )'.");
+                }
 
                 if (linea.Contains("cout") && !linea.Contains("<<"))
+                {
                     erroresSintacticos.Add($"Error Sintáctico (Línea {numLinea}): 'cout' requiere el operador '<<'.");
+                }
 
                 if (linea.Contains("cin") && !linea.Contains(">>"))
+                {
                     erroresSintacticos.Add($"Error Sintáctico (Línea {numLinea}): 'cin' requiere el operador '>>'.");
+                }
             }
-
-            int aperturas = linea.Count(f => f == '{');
-            int cierres = linea.Count(f => f == '}');
-            if (aperturas > 0 && cierres > 0 && linea.IndexOf("{") > linea.LastIndexOf("}"))
-                erroresSintacticos.Add($"Error Sintáctico (Línea {numLinea}): Orden de llaves incorrecto.");
         }
+
+        if (balanceLlaves != 0)
+        {
+            erroresSintacticos.Add("Error Sintáctico: El número de llaves '{' y '}' no coincide en el código.");
+        }
+
         return erroresSintacticos;
     }
 }
